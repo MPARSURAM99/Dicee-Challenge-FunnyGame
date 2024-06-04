23f28602c8e562a4eba393035f79f3e201f2d90e
MPARSURAM <mparsuram99@gmail.come>
2024-06-05 02:45:35 +0530

    All files are rechecked

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById("playBtn").addEventListener("click", () => {
+  
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var imageSource1 = "images/dice" + randomNumber1 + ".png";
+    var image1 = document.querySelector(".img1").setAttribute("src", imageSource1);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var imageSource2 = "images/dice" + randomNumber2 + ".png";
+    var image2 = document.querySelector(".img2").setAttribute("src", imageSource2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 is winner!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 is winner!";
+  } else {
+    document.querySelector("h1").innerHTML = "Match is draw!";
+  }
+});
